5988fbda503b06adf473a3720b60dce5cc560ca1
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-07-24 06:41:01 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let t=document.getElementById('timer');
+let d=new Date().getDate();
+let m=new Date().getMonth();
+let y=new Date().getFullYear()
+let h=new Date().getHours();
+let mi=new Date().getMinutes();
+let s=new Date().getSeconds();
+const ampm = h >= 12 ? 'PM' : 'AM';
+t.innerText=`${d}/${m}/${y}, ${h}:${mi}:${s} ${ampm}`;
